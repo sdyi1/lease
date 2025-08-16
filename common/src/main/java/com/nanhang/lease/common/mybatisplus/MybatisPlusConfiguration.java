ba20f287babadeb816db366d848b4f0718e0c3fd
sdyi1 <2726155142@qq.com>
2025-08-16 16:27:54 +0800

    配置knife4j，mybatisPlus 启动类

--- common/src/main/java/com/nanhang/lease/common/mybatisplus/MybatisPlusConfiguration.java
+++ common/src/main/java/com/nanhang/lease/common/mybatisplus/MybatisPlusConfiguration.java
@@ -3,6 +3,11 @@ package com.nanhang.lease.common.mybatisplus;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author 徐
+ * @description MybatisPlus配置类
+ * @date 2025/8/13
+ */
 @Configuration
 @MapperScan("com.nanhang.lease.web.*.mapper")
 public class MybatisPlusConfiguration {
--- web/wab_admin/src/main/java/com/nanhang/lease/AdminWebApplication.java
+++ web/wab_admin/src/main/java/com/nanhang/lease/AdminWebApplication.java
@@ -2,7 +2,11 @@ package com.nanhang.lease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author 徐
+ * @description 后台管理系统启动类
+ * @date 2025/8/13
+ */
 @SpringBootApplication
 public class AdminWebApplication {
     public static void main(String[] args) {
